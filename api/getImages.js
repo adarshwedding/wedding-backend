46c948ee3f4b42c418fbdf3eca3aa59e1532f39f
gaurav <gauravkrsingh707@gmail.com>
2026-03-04 13:24:13 +0530

    Create getImages.js

---
+++ api/getImages.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+export default async function handler(req, res) {
+
+  const { folder } = req.query;
+
+  try {
+
+    const result = await cloudinary.search
+      .expression(`folder:${folder}`)
+      .sort_by("created_at","desc")
+      .max_results(100)
+      .execute();
+
+    const urls = result.resources.map(r => r.secure_url);
+
+    res.status(200).json(urls);
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch images" });
+  }
+}
